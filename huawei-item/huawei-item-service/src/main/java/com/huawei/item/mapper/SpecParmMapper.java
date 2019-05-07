f0d205a38d7d7d7c726caba8b20685ad12f7c661
CuiBeiJie <15655562705@163.com>
2019-05-07 23:18:52 +0800

    商品规格

--- huawei-item/huawei-item-service/src/main/java/com/huawei/item/mapper/SpecParmMapper.java
+++ huawei-item/huawei-item-service/src/main/java/com/huawei/item/mapper/SpecParmMapper.java
@@ -11,6 +11,7 @@ import tk.mybatis.mapper.common.Mapper;
  * @create: 2019-05-07 20:55
  */
 public interface SpecParmMapper extends Mapper<SpecParam> {
+    //校验同一个规格组内的商品规格参数名称是否重复
     @Select("select * from tb_spec_param where id != #{id} and cid = #{cid} and group_id = #{groupId} and name = #{name} ")
     Integer validateSpecParam(Long id, Long cid, Long groupId,String name);
 }
