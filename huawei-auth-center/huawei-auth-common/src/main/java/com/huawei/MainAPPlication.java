1152b8fae14e3bea0cb9a3aac8794b3c4b5e2580
CuiBeiJie <15655562705@163.com>
2019-06-12 19:18:33 +0800

    解决子模块相互依赖打包时所遇到的问题:依赖的程序包找不到

---
+++ huawei-auth-center/huawei-auth-common/src/main/java/com/huawei/MainAPPlication.java
@@ -0,0 +1,16 @@
+package com.huawei;/**
+ * @Auther: 25349
+ * @Date: 2019/6/12 19:11
+ * @Description:
+ */
+
+/**
+ * @Auther: 25349
+ * @Date: 2019/6/12 19:11
+ * @Description:
+ */
+public class MainAPPlication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ huawei-user/huawei-user-interface/src/main/java/com/huawei/MainApplication.java
@@ -0,0 +1,16 @@
+package com.huawei;/**
+ * @Auther: 25349
+ * @Date: 2019/6/12 19:10
+ * @Description:
+ */
+
+/**
+ * @Auther: 25349
+ * @Date: 2019/6/12 19:10
+ * @Description:
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        
+    }
+}
